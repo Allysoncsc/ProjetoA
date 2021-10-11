e08913c72d8b6bc907935e74a8bdadccec17d179
allysoncsc <allysoncsc@gmail.com>
2021-10-10 21:44:22 -0300

    segundo commit, lista de disciplinas, metodo media.

--- aprenderJava/src/main/java/br/com/projetoA/aprenderJava/Main.java
+++ aprenderJava/src/main/java/br/com/projetoA/aprenderJava/Main.java
@@ -3,6 +3,7 @@ package br.com.projetoA.aprenderJava;
 import javax.swing.JOptionPane;
 
 import br.com.projetoA.aprenderJava.entity.Aluno;
+import br.com.projetoA.aprenderJava.entity.Disciplina;
 
 public class Main {
 
@@ -18,6 +19,27 @@ public class Main {
 	        String cpf = JOptionPane.showInputDialog("Qual o cpf do aluno?");
 	        aluno1.setCPF(cpf);
 	        
+	        /* String disciplina = JOptionPane.showInputDialog("Qual o nome da disciplina?");
+	       aluno1.getDisiciplina().setNomeDisciplina(disciplina);
+	        String nota1 = JOptionPane.showInputDialog("Qual a idade do aluno?");
+	        aluno1.getDisiciplina().setNota1(Double.valueOf(nota1)); */  
+	        // System.out.println("Disciplina : "+aluno1.getDisiciplina().getNomeDisciplina());
+	        //System.out.println("Nta : "+aluno1.getDisiciplina().getNota1());
+	        
+	        Disciplina disciplina1 = new Disciplina();
+	        String disciplina = JOptionPane.showInputDialog("Qual o nome da disciplina?");
+	        disciplina1.setNomeDisciplina(disciplina);
+	        String nota1 = JOptionPane.showInputDialog("Qual a nota 1 do aluno?");
+		    disciplina1.setNota1(Double.valueOf(nota1)); 
+		    String nota2 = JOptionPane.showInputDialog("Qual a nota 2 do aluno?");
+		    disciplina1.setNota1(Double.valueOf(nota2)); 
+		    String nota3 = JOptionPane.showInputDialog("Qual a nota 3 do aluno?");
+		    disciplina1.setNota1(Double.valueOf(nota3)); 
+		    String nota4 = JOptionPane.showInputDialog("Qual a nota 4 do aluno?");
+		    disciplina1.setNota1(Double.valueOf(nota4)); 
+	        
+		    aluno1.getDisiciplinas().add(disciplina1);
+		    
 	        System.out.println("Nome : "+aluno1.getNome());
 		 
 	 }
--- aprenderJava/src/main/java/br/com/projetoA/aprenderJava/entity/Aluno.java
+++ aprenderJava/src/main/java/br/com/projetoA/aprenderJava/entity/Aluno.java
@@ -1,12 +1,24 @@
 package br.com.projetoA.aprenderJava.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aluno {
 
 	private String nome;
 	private int idade;
 	private String CPF;
 	
+	private List <Disciplina> disiciplinas = new ArrayList<Disciplina>();
 	
+	
+	
+	public List<Disciplina> getDisiciplinas() {
+		return disiciplinas;
+	}
+	public void setDisiciplinas(List<Disciplina> disiciplinas) {
+		this.disiciplinas = disiciplinas;
+	}
 	public String getNome() {
 		return nome;
 	}
@@ -25,4 +37,17 @@ public class Aluno {
 	public void setCPF(String cPF) {
 		CPF = cPF;
 	}
+	
+	
+	//teste melhorar
+	public double getMedia() {
+		double somaNotas = 0.0;
+		
+		for (Disciplina disciplina : disiciplinas) {
+			somaNotas += disciplina.getNota1();
+		}
+		
+		return somaNotas/disiciplinas.size();
+	}
+	
 }
