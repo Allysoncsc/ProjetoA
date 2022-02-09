dd4d977461563f5c7178275f0884d82e0a4a1dcb
allysoncsc <allysoncsc@gmail.com>
2022-02-08 23:04:19 -0300

    fim do exercicio employee e incrementa salario com list

--- aprenderJava/src/main/java/br/com/projetoA/aprenderJava/TestaEmployee.java
+++ aprenderJava/src/main/java/br/com/projetoA/aprenderJava/TestaEmployee.java
@@ -14,13 +14,12 @@ public class TestaEmployee {
 
 		System.out.println("How many employees will be registered? ");
 		int n = sc.nextInt();
-		
-		
+
 		List<Employee> list = new ArrayList<>();
-		
+
 		for (int i = 0; i < n; i++) {
 			sc.nextLine();
-			System.out.println("Employee "+ (i+1));
+			System.out.println("Employee " + (i + 1));
 			System.out.println("Id: ");
 			Integer id = sc.nextInt();
 			sc.nextLine();
@@ -28,15 +27,56 @@ public class TestaEmployee {
 			String name = sc.nextLine();
 			System.out.println("Salary: ");
 			Double salary = sc.nextDouble();
-			
+
 			Employee emp = new Employee(id, name, salary);
-			
+
 			list.add(emp);
 		}
-		System.out.println("Enter the employee id that will have salary increase: ");
+		
+		
+		System.out.println("Enter the employee id that will have salary increase: ");	
 		int idsalary = sc.nextInt();
 		
+		//tranformando a list em list stream que aceita funções lambida
+		Employee e = list.stream().filter(x -> x.getId() == idsalary).findFirst().orElse(null);
+		
+				
+		//Integer pos = position(list, idsalary);
+
+		//if (pos == null)
+		if (e == null) {
+			System.out.println("Id inexistente");
+		} else {
+			System.out.println("Enter the percentage: ");
+			double percente = sc.nextDouble();
+			e.increaseSalary(percente);
+			//list.get(pos).increaseSalary(percente);
+
+		}
+		System.out.println();
+		System.out.println("List of employess: ");
+		for (Employee emp : list) {
+			System.out.println(emp.toString());
+		}
+
 		sc.close();
+	}
+
+	
+	public static boolean hasId(List<Employee> list, int id) {
+		Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		//se emp diferente null retorna true
+		return emp !=null;
+	}
+	
+	public static Integer position(List<Employee> list, int id) {
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).getId() == id) {
+				return i;
+			}
+		}
+
+		return null;
 	}
 
 }
